141a116322b6eaf9c58cfb09bc205e4e1d16a788
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2020-08-10 14:07:22 +0200

    Add bundle definition.

---
+++ src/custom/plugins/TkBundleExample/src/Core/Content/Bundle/BundleDefinition.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace TkBundleExample\Core\Content\Bundle;
+
+use Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\PrimaryKey;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\FloatField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\IdField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\StringField;
+use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
+
+class BundleDefinition extends EntityDefinition {
+
+    public const ENTITY_NAME= 'tk_bundle';
+
+    public function getEntityName(): string
+    {
+        return self::ENTITY_NAME;
+    }
+
+    public function getEntityClass(): string
+    {
+        return BundleEntity::class;
+    }
+
+    protected function defineFields(): \Shopware\Core\Framework\DataAbstractionLayer\FieldCollection
+    {
+        return new FieldCollection([
+            (new IdField('id', 'id'))->addFlags(new Required(), new PrimaryKey()),
+            (new StringField('discount_type', 'discountType'))->addFlags(new Required()),
+            (new FloatField('discount', 'discount'))->addFlags(new Required()),
+        ]);
+    }
+}
---
+++ src/custom/plugins/TkBundleExample/src/Core/Content/Bundle/BundleEntity.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace TkBundleExample\Core\Content\Bundle;
+
+use Shopware\Core\Framework\DataAbstractionLayer\Entity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityIdTrait;
+
+class BundleEntity extends Entity {
+
+    use EntityIdTrait;
+
+    /**
+     * @var string
+     */
+    protected $discountType;
+
+    /**
+     * @var float
+     */
+    protected $discount;
+
+    public function getDiscountType(): string
+    {
+        return $this->discountType;
+    }
+
+    public function setDiscountType(string $discountType): void
+    {
+        $this->discountType = $discountType;
+    }
+
+    public function getDiscount(): float
+    {
+        return $this->discount;
+    }
+
+    public function setDiscount(float $discount): void
+    {
+        $this->discount = $discount;
+    }
+
+}
