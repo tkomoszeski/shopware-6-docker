022aba572b5e1a9ce633a1964fd70d5aace0592d
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2020-10-15 11:56:17 +0200

    Adding a bundle badge to the storefront

---
+++ src/custom/plugins/TkBundleExample/src/Storefront/Page/Product/Subscriber/ProductPageCriteriaSubscriber.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+//TODO subscribe to ProductLoaderCriteriaEvent
+//TODO add new association "bundles"
+
+
+
+namespace TkBundleExample\Storefront\Page\Product\Subscriber;
+
+
+use Shopware\Storefront\Page\Product\ProductPageCriteriaEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ProductPageCriteriaSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ProductPageCriteriaEvent::class => 'onProductCriteriaLoaded',
+        ];
+    }
+
+    public function onProductCriteriaLoaded(ProductPageCriteriaEvent $event): void
+    {
+        $event->getCriteria()->addAssociation('bundles.products.cover');
+    }
+}
